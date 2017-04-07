0804d8e32707d00c9ac9e2778a8c1c1e13e0e484
rocaltair <rocaltair@gmail.com>
2017-04-07 21:37:27 +0800

    add variables dump

--- lcoredump.c
+++ lcoredump.c
@@ -13,12 +13,11 @@
 # endif
 #endif
 
-
 #define pushfstring(buf, e, fmt, ...) do{if(sz-e > 0) e+=snprintf(buf+e, sz-e, fmt, ##__VA_ARGS__);}while(0)
 #define LEVELS1	12
 #define LEVELS2	10
 
-#define MAX_BUF_SZ (256 * 1024)
+#define MAX_BUF_SZ (1024 * 1024)
 #define MAX_FRAME_SZ 128
 
 /**
@@ -26,30 +25,32 @@
  */
 
 lua_State *GlobalL = NULL;
+static int is_full_stack = 0;
 
-static size_t dump_lua_traceback(lua_State *L, char *buf, size_t sz)
+static size_t dump_lua_traceback(lua_State *L, char *buf, size_t sz, int is_show_var)
 {
 	size_t e = 0;
 	int level = 1;
 	int firstpart = 1;
 	lua_Debug ar;
+	int i = 0;
 
-	pushfstring(buf, e, "traceback:");
+	pushfstring(buf, e, "%s traceback:", is_show_var ? "full" : "");
 	while (lua_getstack(L, level++, &ar)) {
 		if (level > LEVELS1 && firstpart) {
 			if (!lua_getstack(L, level+LEVELS2, &ar))
 				level--;
 			else {
-				pushfstring(buf, e, "%s", "\n\t...");
+				pushfstring(buf, e, "%s", "\n...");
 				while (lua_getstack(L, level+LEVELS2, &ar))
 					level++;
 			}
 			firstpart = 0;
 			continue;
 		}
-		pushfstring(buf, e, "\n\t");
+		pushfstring(buf, e, "\n");
 		lua_getinfo(L, "Snl", &ar);
-		pushfstring(buf, e, "%s:", ar.short_src);
+		pushfstring(buf, e, "[%d]%s:", level - 2, ar.short_src);
 		if (ar.currentline > 0)
 			pushfstring(buf, e, "%d:", ar.currentline);
 		if (*ar.namewhat != '\0')
@@ -62,6 +63,35 @@ static size_t dump_lua_traceback(lua_State *L, char *buf, size_t sz)
 			else
 				pushfstring(buf, e, " in function <%s:%d>",
 					    ar.short_src, ar.linedefined);
+		}
+
+		i = 1;
+		while(is_show_var) {
+			int type;
+			lua_Debug arf;
+			const char *name = lua_getlocal(L, &ar, i++);
+			const char *typename;
+			if (name == NULL)
+				break;
+			type = lua_type(L, -1);
+			typename = lua_typename(L, type);
+			pushfstring(buf, e, "\n\t%s(%s) : ", name, typename);
+			switch(type) {
+			case LUA_TFUNCTION:
+				lua_getinfo(L, ">Snl", &arf);
+				if (*arf.what == 'C' || *arf.what == 't')
+					pushfstring(buf, e, "%s@C",
+						    (arf.name != NULL ? arf.name : "defined"));
+				else
+					pushfstring(buf, e, "%s@%s:%d",
+						    (arf.name != NULL ? arf.name : "defined"),
+						    arf.short_src, arf.linedefined);
+				break;
+			default:
+				pushfstring(buf, e, "%s", lua_tostring(L, -1));
+				lua_pop(L, 1);
+				break;
+			}
 		}
 	}
 	pushfstring(buf, e, "\n");
@@ -85,7 +115,7 @@ static void signal_handler(int signum)
 	signal(signum, SIG_DFL);
 
 	buf = malloc(MAX_BUF_SZ);
-	dump_lua_traceback(L, buf, MAX_BUF_SZ);
+	dump_lua_traceback(L, buf, MAX_BUF_SZ, is_full_stack);
 	fprintf(stderr, "%s", buf);
 	fflush(stderr);
 	free(buf);
@@ -109,12 +139,20 @@ static int lua__freopen(lua_State *L)
 	return 0;
 }
 
+static int lua__set_show_var(lua_State *L)
+{
+	is_full_stack = lua_toboolean(L, 1);
+	return 0;
+}
+
+
 
 int luaopen_lcoredump(lua_State* L)
 {
 	luaL_Reg lfuncs[] = {
 		{"register", lua__register},
 		{"freopen", lua__freopen},
+		{"set_show_var", lua__set_show_var},
 		{NULL, NULL},
 	};
 	luaL_newlib(L, lfuncs);
--- test.lua
+++ test.lua
@@ -2,17 +2,18 @@ lcoredump = require "lcoredump"
 lcoredump.register(2)
 lcoredump.register(3)
 lcoredump.register(15)
+lcoredump.set_show_var(true)
 
 -- if freopen invoked, traceback would be write to file traceback.log
 -- lcoredump.freopen("traceback.log")
 
-function foo()
+function foo(tbl, num, str)
 	print("press Ctrl-C(SIGINT) or Ctrl-\\(SIGTERM) please, traceback would be dumped to stderr")
 	io.read()
 end
 
-function bar()
-	foo()
+function bar(func, s)
+	foo({}, 1, "fdsa")
 end
 
-bar()
+bar(foo, "hello world!")
